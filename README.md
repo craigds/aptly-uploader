ff0a630a6c3f114f1bae69667faea130ed3f0cfd
Craig de Stigter <craig.destigter@koordinates.com>
2019-07-25 14:33:41 +1200

    Require Python 3.6, use nice shiny things

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ APTLY_UNAME=build_uploader
 APTLY_PASSWD="Kāore i kō mai i kō atu i a koe"
 aptly-upload --aptly-url https://aptly.local --repo mybuilds --series trusty *.deb
 ```
+
+# requirements
+
+Python 3.6+
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='aptly-uploader',
-    version='0.3.2',
+    version='0.4',
     description='Upload deb files to aptly',
     url='https://github.com/craigds/aptly-uploader',
     author='Craig de Stigter',
@@ -18,10 +18,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     packages=[],
